bccce0c7a9599d8bf4894263f8c9f3b64757819a
subramanya-h2199 <75600655+subramanya-h2199@users.noreply.github.com>
2021-03-17 16:10:47 +0530

    tables related to development are configured

--- lambda_function.py
+++ lambda_function.py
@@ -5,10 +5,11 @@ import os
 import logging
 
 if os.environ['ENVIRONMENT'].upper() =="DEVELOPMENT":
-    place_table = "localzi-place-rating"
-    user_table = "localzi-user-interestcards"
+    place_table = "localzi-place-rating-test"
+    user_table = "localzi-user-interest-test"
     recommender_table ="localzi-places-recommended-test"
-    user = "u1"
+    user = os.environ['USERID']
+    #for of now the userid is given manually later its dynamically assigned
 elif os.environ['ENVIRONMENT'].upper() =="PRODUCTION":
     place_table = "localzi-place-rating"
     user_table = "localzi-user-interestcards"
@@ -18,30 +19,27 @@ elif os.environ['ENVIRONMENT'].upper() =="PRODUCTION":
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-
 db=boto3.resource('dynamodb')
-
 try:
-    place_category_rating=db.Table(place_table)
+    place_interest_rating=db.Table(place_table)
 except Exception as e:
     logger.error(e)
-response = place_category_rating.scan()
+response = place_interest_rating.scan()
 
 def lambda_handler(event=None, context=None):
-
     PlaceID=[]
-    CategoryID=[]
+    InterestID=[]
     Rating=[]
     try:
         for place in response['Items']:
-            PlaceID.append(place['PlaceID'])
-            CategoryID.append(place['CategoryID'])
-            Rating.append(place['Rating'])
+            PlaceID.append(place['placeID'])
+            InterestID.append(place['interestID'])
+            Rating.append(place['rating'])
     except Exception as e:
         logger.error(e)
 
-    placedf = pd.DataFrame(list(zip(PlaceID, CategoryID, Rating)), columns= ['PlaceID', 'CategoryID', 'Rating'])
-    PlaceID= CategoryID = Rating = None
+    placedf = pd.DataFrame(list(zip(PlaceID, InterestID, Rating)), columns= ['PlaceID', 'InterestID', 'Rating'])
+    PlaceID= InterestID = Rating = None
     res=each_user_places_rec(user, placedf, user_table, recommender_table)
     if res:
         logger.info("successfully ran for "+str(user))
--- recommender.py
+++ recommender.py
@@ -3,17 +3,18 @@ import boto3
 from boto3.dynamodb.conditions import Key
 import logging
 
-
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
 def each_user_places_rec(user,placedf=None,user_table=None,recommender_table=None):
     group_by_placeid = placedf.groupby(placedf.PlaceID)
     unique_placeid = list(set(placedf['PlaceID']))
     userdf=fetch_user_details(user, user_table)
     di = []
+
     for place in unique_placeid:
         each_place = group_by_placeid.get_group(place)
-        merge_each_user_place = pd.merge(userdf, each_place, on='CategoryID')
+        merge_each_user_place = pd.merge(userdf, each_place, on='InterestID')
         di.append((place, sum(merge_each_user_place['Rating'])))
 
     di.sort(key=lambda x: x[1], reverse=True)
@@ -25,14 +26,16 @@ def results_into_dynamo(user,placedf,sorted_di,recommender_table):
     db = boto3.resource('dynamodb')
     tab = db.Table(recommender_table)
     try:
-        for i in range(10):
+        for i in range(5):
             tab.put_item(
                 Item={
                     'UserID': user,
                     'PlaceID': str(placedf.loc[placedf['PlaceID'] == sorted_di[i][0], 'PlaceID'].iloc[0])
                 })
+
     except Exception as exp:
         logger.error(exp)
+        #print(str(placedf.loc[placedf['PlaceID'] == sorted_di[i][0], 'PlaceID'].iloc[0]))
     return 1
 
 
@@ -40,16 +43,18 @@ def fetch_user_details(user,user_table):
     try:
         dynamodb = boto3.resource('dynamodb')
         table = dynamodb.Table(user_table)
-        response = table.query(KeyConditionExpression=Key('UserID').eq(user))
+        response = table.query(KeyConditionExpression=Key('userID').eq(user))
     except Exception as exp:
         logger.error(exp)
     UserID=[]
-    CategoryID=[]
+    InterestID=[]
     try:
         for user in response['Items']:
-            UserID.append(user['UserID'])
-            CategoryID.append(user['CategoryID'])
+            UserID.append(user['userID'])
+            InterestID.append(user['interestCardID'])
     except Exception as exp:
         logger.error(exp)
-    userdf=pd.DataFrame(list(zip(UserID, CategoryID)), columns=['UserID', 'CategoryID'] )
+    userdf=pd.DataFrame(list(zip(UserID, InterestID)), columns=['UserID', 'InterestID'] )
+    #print("user details are")
+    #print(UserID, InterestID)
     return userdf
