1b0be03c39c1c81f44f452ff41ece392764a993e
subramanya-h2199 <75600655+subramanya-h2199@users.noreply.github.com>
2021-03-25 19:17:31 +0530

    dynamodb trigger added

--- lambda_function.py
+++ lambda_function.py
@@ -5,46 +5,71 @@ import os
 import logging
 
 if os.environ['ENVIRONMENT'].upper() =="DEVELOPMENT":
-    place_table = "localzi-place-rating-test"
+    place_table= "localzi-place-rating-test"
     user_table = "localzi-user-interest-test"
     recommender_table ="localzi-user-place-recommendation-test"
-    user = os.environ['USERID']
     #for of now the userid is given manually later its dynamically assigned
 elif os.environ['ENVIRONMENT'].upper() =="PRODUCTION":
     place_table = "localzi-place-rating"
     user_table = "localzi-user-interestcards"
     recommender_table = "localzi-user-place-recommendation"
-    user = "u1"
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-db=boto3.resource('dynamodb')
+dynamodb=boto3.resource('dynamodb')
 try:
-    place_interest_rating=db.Table(place_table)
+    place_interest_rating=dynamodb.Table(place_table)
 except Exception as e:
     logger.error(e)
-response = place_interest_rating.scan()
+response_placedf = place_interest_rating.scan()
 
 def lambda_handler(event=None, context=None):
-    PlaceID=[]
-    InterestID=[]
-    Rating=[]
+    global response_placedf
+    if event['Records'][0]['eventSourceARN'] == 'arn:aws:dynamodb:ap-south-1:895767246702:table/localzi-user-interest-test/stream/2021-03-22T04:30:35.232':
+        if event['Records'][0]['eventName']== 'INSERT' or event['Records'][0]['eventName']== 'MODIFY':
+            user = event['Records'][0]['dynamodb']['NewImage']['userID']['S']
+            logger.info(user)
+            del_user_if_exist_in_recommender_table(user)
+            PlaceID=[]
+            InterestID=[]
+            Rating=[]
+            try:
+                for place in response_placedf['Items']:
+                    PlaceID.append(place['placeID'])
+                    InterestID.append(place['interestID'])
+                    Rating.append(place['rating'])
+            except Exception as e:
+                logger.error(e)
+
+            placedf = pd.DataFrame(list(zip(PlaceID, InterestID, Rating)), columns= ['PlaceID', 'InterestID', 'Rating'])
+            PlaceID= InterestID = Rating = None
+            res=each_user_places_rec(user, placedf, user_table, recommender_table)
+            if res:
+                logger.info("successfully ran for "+str(user))
+            else:
+                logger.info("failed for "+str(user))
+                
+        elif event['Records'][0]['eventName']== 'REMOVE':
+            user = event['Records'][0]['dynamodb']['OldImage']['userID']['S']
+            table = dynamodb.Table(recommender_table)
+            response = table.query(KeyConditionExpression=Key('userID').eq(user))
+            delete_recommended_of_user(response, table)
+
+
+def del_user_if_exist_in_recommender_table(user):
     try:
-        for place in response['Items']:
-            PlaceID.append(place['placeID'])
-            InterestID.append(place['interestID'])
-            Rating.append(place['rating'])
-    except Exception as e:
+        table = dynamodb.Table(recommender_table)
+        response = table.query(KeyConditionExpression=Key('userID').eq(user))
+        if len(response['Items'])>0:
+            delete_recommended_of_user(response,table)
+    except Exception as exp:
+        logger.error(exp)
+
+def delete_recommended_of_user(response_query,table):
+    try:
+        for del_user in response_query['Items']:
+            response = table.delete_item(Key={'userID': del_user['userID'], 'placeID': del_user['placeID']})
+        logger.info("deleted successfully {}".format(response_query['Items'][0]['userID']))
+    except:
         logger.error(e)
-
-    placedf = pd.DataFrame(list(zip(PlaceID, InterestID, Rating)), columns= ['PlaceID', 'InterestID', 'Rating'])
-    PlaceID= InterestID = Rating = None
-    res=each_user_places_rec(user, placedf, user_table, recommender_table)
-    if res:
-        logger.info("successfully ran for "+str(user))
-    else:
-        logger.info("failed for "+str(user))
-
-
-
--- recommender.py
+++ recommender.py
@@ -18,32 +18,32 @@ def each_user_places_rec(user,placedf=None,user_table=None,recommender_table=Non
         di.append((place, sum(merge_each_user_place['Rating'])))
 
     di.sort(key=lambda x: x[1], reverse=True)
-    res=results_into_dynamo(user,placedf, di,recommender_table)
+    print("final destination:",di)
+    res=results_into_dynamo(user, di,recommender_table)
     return 1
 
 
-def results_into_dynamo(user,placedf,sorted_di,recommender_table):
+def results_into_dynamo(user_id,sorted_di,recommender_table):
     db = boto3.resource('dynamodb')
     tab = db.Table(recommender_table)
     try:
         for i in range(5):
             tab.put_item(
                 Item={
-                    'userID': user,
-                    'placeID': str(placedf.loc[placedf['PlaceID'] == sorted_di[i][0], 'PlaceID'].iloc[0])
+                    'userID': user_id,
+                    'placeID': str(sorted_di[i][0])
                 })
 
     except Exception as exp:
         logger.error(exp)
-        #print(str(placedf.loc[placedf['PlaceID'] == sorted_di[i][0], 'PlaceID'].iloc[0]))
     return 1
 
 
-def fetch_user_details(user,user_table):
+def fetch_user_details(user_id,user_table):
     try:
         dynamodb = boto3.resource('dynamodb')
         table = dynamodb.Table(user_table)
-        response = table.query(KeyConditionExpression=Key('userID').eq(user))
+        response = table.query(KeyConditionExpression=Key('userID').eq(user_id))
     except Exception as exp:
         logger.error(exp)
     UserID=[]
@@ -55,6 +55,4 @@ def fetch_user_details(user,user_table):
     except Exception as exp:
         logger.error(exp)
     userdf=pd.DataFrame(list(zip(UserID, InterestID)), columns=['UserID', 'InterestID'] )
-    #print("user details are")
-    #print(UserID, InterestID)
     return userdf
